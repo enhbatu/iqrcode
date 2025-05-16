5b998bf1ce4899596442e533afb11adb813b9654
Enkhbat Ulziibat <enhbatu@gmail.com>
2025-05-16 08:36:35 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { QRCodeComponent } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    QRCodeComponent,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'iqrcode';
+  inputText = '';
+  qrcode = '';
+  logoUrl = '';
+  setQR() {
+    this.qrcode=this.inputText;
+  }
 }
